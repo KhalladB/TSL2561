4880c3493d0d0f700c854cbc9408ad998d807f0d
KhalladB <eng.khallad@hotmail.com>
2016-11-17 13:38:08 +0300

    Update openag_tsl2561.h

--- openag_tsl2561.h
+++ openag_tsl2561.h
@@ -95,7 +95,7 @@ class Tsl2561: public Module {
   // form tsl2561_Sensor
   int lux_; // lux
   float par_; // (umol)*(m^-2)*(s^-1)
-
+  unsigned long lux;
   
   private:
   // Added from dht22 example
@@ -105,7 +105,7 @@ class Tsl2561: public Module {
   void writeRegister(int deviceAddress, int address, uint8_t val);
   
   //
-  void readSensorData();
+  float readSensorData();
   
   
   // Added from the original code
@@ -118,7 +118,6 @@ class Tsl2561: public Module {
   unsigned int b;
   unsigned int m;
   unsigned long temp;
-  unsigned long lux;
   
  // Added from sensor_tsl2561
   String lux_instruction_code_;
