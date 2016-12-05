089a9c22f5df315ae47b934ae0a91a230bc65311
KhalladB <eng.khallad@hotmail.com>
2016-12-05 12:07:15 +0300

    Removed Some Comments

--- openag_tsl2561.cpp
+++ openag_tsl2561.cpp
@@ -82,7 +82,7 @@ void Tsl2561::readSensorData()
     if(ch0/ch1 < 2 && ch0 > 4900)
     {
       lux_ = -1;
-      return;  //ch0 out of range, but ch1 not. the lux is not valid in this situation.
+      return;  //ch0 out of range, but ch1 not. the lux is not valid in this situation
     }
     lux_average += (float) calculateLux(0, 0, 0);
   }
